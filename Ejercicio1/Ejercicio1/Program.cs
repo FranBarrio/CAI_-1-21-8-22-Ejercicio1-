fd16aae96b9cf432573240165b4ceae4e0c99ed1
FranBarrio <franchibarrio12@gmail.com>
2022-08-21 14:33:30 -0300

    Inicio del Proyecto
    Primer set de cambios correspondientes al Ejercicio1

---
+++ Ejercicio1/Ejercicio1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 239; i++)
+            {
+                Console.Write(i);
+                if (i % 3 == 0)
+                {
+                    Console.Write("Foo");
+                }
+                if (i % 5 == 0)
+                {
+                    Console.Write("Bar");
+                }
+                Console.Write("\n");
+            }
+            Console.WriteLine("Presione cualquier tecla para finalizar.");
+            Console.ReadKey();
+        }
+    }
+}
